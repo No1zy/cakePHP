86da51e1a56d220e4faa75ff7b2259a809fd7cfb
no1zy <no1zy@MBP.local>
2017-04-18 23:35:55 +0900

    script修正

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# dev
+# cakePHP 2.9.7 for docker
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!bin/sh
 
+MYSQL_DATABASE_NAME='cake'
 docker-compose up -d 
-#docker exec -it $(docker-compose ps -q cakedb) mysql -u root -e "CREATE USER 'root'@'%';"
-
+docker exec -it $(docker-compose ps -q cakedb) mysql -u root -e "create database $MYSQL_DATABASE_NAME;"
 docker exec -it $(docker-compose ps -q cakedb) mysql -u root -e "grant all privileges on *.* to root@'%' with grant option;"
 
