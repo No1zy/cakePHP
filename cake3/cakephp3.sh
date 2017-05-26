58770a4303d5c3b3619a8cee344e42bad663c48d
No1zy <bgkn12601208@gn.iwasaki.ac.jp>
2017-05-27 03:10:14 +0900

    modified cakephp3.sh

--- cake3/cakephp3.sh
+++ cake3/cakephp3.sh
@@ -9,7 +9,7 @@ yum update -y
 yum install -y epel-release
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
 yum install -y --enablerepo=remi,remi-php70 php php-devel php-mbstring php-ldap php-mysql php-pdo php-gd php-cli php-opcache php-mcrypt php-intl
-yum install -y mysql mariadb-server httpd git
+yum install -y mysql mariadb-server httpd git vim
 
 systemctl enable httpd
 systemctl enable mariadb
@@ -21,8 +21,10 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 mkdir $DOCUMENT_ROOT/$APP_DIR
 
-echo "Y" | composer create-project --prefer-dist cakephp/app $DOCUMENT_ROOT/$APP_DIR
-
+composer create-project --prefer-dist cakephp/app $DOCUMENT_ROOT/$APP_DIR <<EOS
+Y
+Y
+EOS
 
 firewall-cmd --add-service=http --permanent
 firewall-cmd --reload
@@ -45,7 +47,7 @@ cp $DOCUMENT_ROOT/$APP_DIR/config/app.default.php $DOCUMENT_ROOT/$APP_DIR/config
 sed -ie "232 s/my_app/$DB_USER/" $DOCUMENT_ROOT/$APP_DIR/config/app.php
 sed -ie "233 s/secret/$DB_PASSWORD/" $DOCUMENT_ROOT/$APP_DIR/config/app.php
 sed -ie "234 s/my_app/$DB_NAME/" $DOCUMENT_ROOT/$APP_DIR/config/app.php
-sed -ie "236 s/UTC/Asia\/Tokyo/" app.php
-
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u $DB_USER
+sed -ie "236 s/UTC/Asia\/Tokyo/" $DOCUMENT_ROOT/$APP_DIR/config/app.php
+sed -ie "s/__SALT__/hogefugavar/" $DOCUMENT_ROOT/$APP_DIR/config/app.php
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u $DB_USER mysql
 
